408a329389281f788946d399c56d42401716ff8c
trupti-soni-c <sonitrupti701@gmail.com>
2021-05-01 10:26:11 +0530

    dynamic site

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ServicesComponent } from './services/services.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutmeComponent,
+    ServicesComponent,
+    MySkillsComponent,
+    MyWorkComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  navigationArray=[ {'menulink':'#slide01','menuname':'Home','data-menuanchor':'slide01'},
+                    {'menulink':'#slide02','menuname':'About Me','data-menuanchor':'slide02'},
+                    {'menulink':'#slide03','menuname':'Services','data-menuanchor':'slide03'},
+                    {'menulink':'#slide04','menuname':'My Skills','data-menuanchor':'slide04'},
+                    {'menulink':'#slide05','menuname':'My Work','data-menuanchor':'slide05'},
+                    {'menulink':'#slide06','menuname':'Testimonials','data-menuanchor':'slide06'},
+                    {'menulink':'#slide07','menuname':'Contact Me','data-menuanchor':'slide07'}
+];
+}
